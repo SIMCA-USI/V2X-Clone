f8359e2a6b536455ebd1b6a612c413d294d7f01d
ait0r <aitorgt77@gmail.com>
2019-04-08 15:30:29 +0200

    Subida del script

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # V2X-Clone
-Script para clonar los módulos
+Script para instalar y clonar los módulos.
+Imagen de referencia a usar: https://drive.upm.es/index.php/f/168534976
+
+# Uso
+El script tiene que ser ejecutado como superusuario
+
+```console
+$ sudo python3 script.py -i {IMAGEN} -o {DISCO SD} -id {ID DEL MÓDULO}
+```
+
+Ejemplo:
+```console
+$ sudo python3 script.py -i base_module.iso -o /dev/sdd -id 2
+```
+
+# Configuración
+Hostname: V2X-{ID}
+## WIFI
+SSID: V2X-{ID} Ejemplo: V2X-11, V2X-2
+PWD: V2X_stati0n
+
+## Cable
+DHCP por defecto trás la instalación
+IP estática en la red 192.168.0.0/24 donde el número de terminal es el ID del módulo.
+Ejemplo: 192.168.0.11 para V2X-11, 192.168.0.2 para V2X-2 
+
+# Login
+root:debian
---
+++ v2xclone/script.py
@@ -0,0 +1,156 @@
+import argparse
+import subprocess
+from pathlib import Path
+
+tmp: Path = Path('/tmp/clone')
+TITLE = '*' * 10
+
+
+def umount(unit: str) -> None:
+    """
+    Function to unmount a unit.
+
+    :param unit: Unit to unmount.
+    :type unit: str
+    """
+
+    try:
+        print(f'{TITLE}  Desmontando unidad {unit}  {TITLE}')
+        subprocess.run(['umount', f'{unit}'])
+    except Exception:
+        print(f"No se pudo desmontar la unidad {unit}")
+        exit(-1)
+    else:
+        print(f'{TITLE}  Unidad desmontada  {TITLE}')
+
+
+def clone(image: str, unit: str) -> None:
+    """
+    Function to clone a image to the SD card
+
+    :param image: The .iso image
+    :param unit: The target unit
+    """
+
+    cmd_list = ['dd', f'if={image}', f'of={unit}', 'bs=1M', 'status=progress']
+    try:
+        print(f'{TITLE}  Clonando imagen {args.image}  {TITLE}')
+        dd = subprocess.Popen(cmd_list)
+        dd.wait()
+
+    except Exception:
+        print(f"No se pudo clonar la imagen {args.image}")
+        exit(-1)
+    else:
+        print(f'{TITLE}  Imagen clonada  {TITLE}')
+
+
+def format_unit(unit: str, name: str = '') -> None:
+    """
+    Function to format a SD CARD
+
+    :param unit: The target unit
+    :param name: Label name
+    """
+    format_list = ['mkfs.ext4', '-F', '-m 0', f'-L V2X-{name}', f'{unit}']
+
+    try:
+        print(f'{TITLE}  Formateando unidad {unit}  {TITLE}')
+        subprocess.run(format_list)
+    except Exception:
+        print(f"No se pudo desmontar la unidad {unit}")
+    else:
+        print(f'{TITLE}  Unidad desmontada  {TITLE}')
+
+
+def mount(unit: str) -> None:
+    """
+    Function to mount a SD card
+
+    :param unit: Target
+    """
+    global tmp
+
+    try:
+        print(f'{TITLE}  Montando unidad {args.card}  {TITLE}')
+        if not tmp.exists():
+            subprocess.run(['mkdir', '/tmp/clone'])
+        subprocess.run(['mount', f'{unit}1', '/tmp/clone'])
+    except Exception:
+        print(f"No se pudo montar la unidad {unit}")
+    else:
+        print(f'{TITLE}  Unidad Montada en {tmp}  {TITLE}')
+
+
+def edit_files() -> None:
+    """
+    Function to edit the files needed to the new station
+    """
+
+    HOSTNAME = tmp.joinpath('etc', 'hostname')
+    HOSTAPD = tmp.joinpath('etc', 'hostapd', 'hostapd.conf')
+    HOSTS = tmp.joinpath('etc', 'hosts')
+    RULES = tmp.joinpath('etc', 'udev', 'rules.d')
+
+    # Hostname must be equal to the host local loopback
+    print(f"Escribiendo {HOSTNAME}")
+    with open(str(HOSTNAME), 'w') as file:
+        file.write(f'V2X-{args.id}')
+
+    print(f"Escribiendo {HOSTS}")
+    with open(str(HOSTS), 'w') as file:
+        file.write('127.0.0.1	localhost\n')
+        file.write(f'127.0.1.1	V2X-{args.id}\n')
+        file.write('::1     localhost ip6-localhost ip6-loopback\n')
+        file.write('ff02::1 ip6-allnodes\n')
+        file.write('ff02::2 ip6-allrouters\n')
+
+    # 2.4 GHZ WIFI configuration
+    print(f"Escribiendo {HOSTAPD}")
+    with open(str(HOSTAPD), 'w') as file:
+        file.write('interface=wlan0\n')
+        file.write('driver=nl80211\n')
+        file.write(f'ssid=V2X-{args.id}\n')
+        file.write('channel=1\n')
+        file.write('wpa=3\n')
+        file.write('wpa_key_mgmt=WPA-PSK\n')
+        file.write('wpa_passphrase=V2X_stati0n\n')
+        file.write('rsn_pairwise=CCMP\n')
+
+    subprocess.run(['rm', '-rf', str(RULES)+'/*'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--image", type=Path, help="Image to clone", required=True)
+    parser.add_argument("-o", "--card", type=Path, help="Target card", required=True)
+    parser.add_argument("-id", "--id", type=int, help="Module ID", required=True)
+
+    args = parser.parse_args()
+    args.image: Path
+    args.card: Path
+
+    if not args.image.exists():
+        print(f"El archivo {args.image} no existe")
+        exit(-1)
+
+    if not args.image.is_file():
+        print(f"{args.image} no es un archivo")
+        exit(-1)
+
+    if not args.card.exists():
+        print(f"El directorio {args.card} no existe")
+        exit(-1)
+
+    if args.card.is_file():
+        print(f"{args.image} es un archivo")
+        exit(-1)
+
+    umount(str(args.card) + '1')
+    format_unit(str(args.card))
+    clone(str(args.image), str(args.card))
+    mount(str(args.card))
+    edit_files()
+    umount(str(tmp))
+
+    print("Clonación finalizada")
